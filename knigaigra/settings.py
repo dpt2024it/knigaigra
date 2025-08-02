767a8b7b072830ba4ea3e4e2fec3d2ee1f7366d8
dpt2024it <dpt2024@students.softuni.bg>
2025-08-02 16:21:20 +0300

    Switch database from SQLite to PostgreSQL in settings.py

--- knigaigra/settings.py
+++ knigaigra/settings.py
@@ -73,10 +73,15 @@ WSGI_APPLICATION = "knigaigra.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db3knigaigra",
+        "USER": "postgres",
+        "PASSWORD": "sU2025",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
